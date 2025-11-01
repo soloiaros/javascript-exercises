8cf1eaf16da4166a4bfd01638719177899651603
soloiaros <soloiaros@gmail.com>
2025-11-01 13:32:57 -0700

    Palindromes assignment complete

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let strippedString = '';
+  for (char of string.toLowerCase()) {
+    if ((char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')) {strippedString += char};
+  };
+  return strippedString === strippedString.split('').reverse().join('');
 };
 
 // Do not edit below this line
