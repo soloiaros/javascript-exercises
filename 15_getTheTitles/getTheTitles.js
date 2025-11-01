00216faa63a6780fd21acc72dadc3940a84204ef
soloiaros <soloiaros@gmail.com>
2025-11-01 14:10:10 -0700

    Titles assgnment complete.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  return books.reduce((ac, cu) => {
+    ac.push(cu.title);
+    return ac;
+  }, [])
 };
 
 // Do not edit below this line
