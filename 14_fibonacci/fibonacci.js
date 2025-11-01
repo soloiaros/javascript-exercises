3363725f7d216b729a35d6856f4a650dddfe6f8f
soloiaros <soloiaros@gmail.com>
2025-11-01 13:57:05 -0700

    Fibonacci assignment complete.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return 'OOPS';
 
+  let lastEntries = [0, 1];
+  if (n == 0 || n == 1) {return lastEntries[n]} else {
+    for (let i = 1; i < n; i++) {
+      lastEntries.push(lastEntries[0] + lastEntries[1]);
+      lastEntries.shift(1);
+    };
+    return lastEntries[1];
+  };
 };
 
 // Do not edit below this line
