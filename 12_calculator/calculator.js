a555a048c0848b7383b94c6974043b06daf32c30
soloiaros <soloiaros@gmail.com>
2025-11-01 12:57:30 -0700

    Calculator assignment complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	return numberArray.reduce((ac, cu) => ac + cu, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  return numberArray.reduce((ac, cu) => ac * cu);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {return 1} else {
+    factorialNumbers = [];
+    for (let i = 1; i <= number; i++) factorialNumbers.push(i);
+    return factorialNumbers.reduce((ac, cu) => ac * cu);
+  }
 };
 
 // Do not edit below this line
